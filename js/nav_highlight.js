d9eabe64836871299598305f5a6e97c83bbfe3a0
lisa14o <mine@Brian-Noyes-Watkinss-iMac.local>
2016-03-02 17:29:18 -0800

    video page and fragments

---
+++ js/nav_highlight.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var str=location.href.toLowerCase();
+	$("#drawing_items li a").each(function() {
+	if (str.indexOf(this.href.toLowerCase()) > -1) {
+		$("li.highlight").removeClass("highlight");
+		$(this).parent().addClass("highlight");
+	}
+
+ });
+
+ })
